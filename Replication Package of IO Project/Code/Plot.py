933ebb97f775c46ca5208d15c5d5491ee077203a
JingxiangH <161834476+JingxiangH@users.noreply.github.com>
2024-05-14 16:23:00 +0800

    1

--- Replication Package of IO Project/Code/Plot.py
+++ Replication Package of IO Project/Code/Plot.py
@@ -1,36 +1,29 @@
 import os
 import platform
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.font_manager import FontProperties
+from io import StringIO
 
 # Set working directories
 paths = {
-    "Darwin": "/Users/huangjingxiang/Library/CloudStorage/Dropbox/大三下/Coding Sample/Replication Package of IO Project/Code/",
-    "Windows": "C:\\Users\\iamja\\Dropbox\\大三下\\Coding Sample\\Replication Package of IO Project\\Code\\"
+    "Darwin": "/Users/huangjingxiang/Library/CloudStorage/Dropbox/大三下/Coding Sample/Replication Package of IO Project/",
+    "Windows": "C:\\Users\\iamja\\Dropbox\\大三下\\Coding Sample\\Replication Package of IO Project\\"
 }
 
 new_path = paths.get(platform.system(), os.getcwd())
 os.chdir(new_path)
 print(f"Intended working directory: {os.getcwd()}")
 
-chinese_font = FontProperties(fname='./Songti.ttc')
+chinese_font = FontProperties(fname='./Code/Songti.ttc')
 
 ########### China PV New Installed Capacity and Forecast.png
 
-# 数据点
-years = np.array([2011, 2012, 2013, 2014, 2015, 
-                  2016, 2017, 2018, 2019, 2020, 
-                  2021, 2022, 2023, 2024, 2025, 
-                  2027, 2030])
-conservative_scenario = np.array([1, 3, 10, 11, 17, 
-                                  31, 52, 42, 30, 48,
-                                  53, 88, 95, 95, 101, 
-                                  108, 120])
-optimistic_scenario = np.array([1, 3, 10, 11, 17, 
-                                  31, 52, 42, 30, 48,
-                                  53, 88, 120, 120, 125, 
-                                  130, 140])
+Data_Installed_Capacity = pd.read_csv('./Data/装机量.csv')
+years = Data_Installed_Capacity["Year"]
+conservative_scenario = Data_Installed_Capacity["Conservative_Scenario"]
+optimistic_scenario = Data_Installed_Capacity["Optimistic_Scenario"]
 
 # 绘制图表
 plt.figure(figsize=(6, 4))
@@ -44,6 +37,32 @@ plt.grid(True)
 plt.xticks(ticks=np.arange(2011, 2031, 3), labels=np.arange(2011, 2031, 3))  # 每隔三年显示一次
 plt.xlim(2011, 2030)
 
-path = "./China PV New Installed Capacity and Forecast.png"
+path = "./Figure/China PV New Installed Capacity and Forecast.png"
 plt.savefig(path, dpi=300)
 plt.close()
+
+########### World PV New Installed Capacity and Forecast.png
+
+# 使用StringIO读取CSV数据
+data = pd.read_csv('./Data/1_全球装机预测.csv')
+
+# 提取类别和各区域数据
+years = data['Year']
+regions = data.columns[1:]
+
+# 绘制图表
+plt.figure(figsize=(8, 5))
+for region in regions:
+    plt.plot(years, data[region], label=region)
+
+# plt.title('全球各国各地区的光伏新增装机量 (单位: GW)', fontproperties=chinese_font)
+plt.xlabel('年份', fontproperties=chinese_font)
+plt.ylabel('装机量（GW）', fontproperties=chinese_font)
+plt.legend(prop=chinese_font, bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.grid(True)
+plt.xticks(ticks=np.arange(years.min(), years.max()+1, 1), labels=np.arange(years.min(), years.max()+1, 1), fontproperties=chinese_font)  # 每年显示一次
+plt.xlim(years.min(), years.max())
+
+# 保存图表，增加DPI以提高清晰度
+path = "./Figure/World PV New Installed Capacity and Forecast.png"
+plt.savefig(path, dpi=300, bbox_inches='tight')
